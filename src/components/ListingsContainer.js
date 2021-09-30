64d968bfa91e3d21e3b9683d195eda797ccf0c27
Alex Aguirre <aguirre_alex@rocketmail.com>
2021-09-30 04:33:18 -0500

    core complete

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,11 +1,36 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, {useState, useEffect} from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
+function ListingsContainer({search}) {
+  const [listings, setListings] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+    .then((res) => res.json())
+    .then((listings) => setListings(listings));
+  }, [])
+
+  function handleDeleteListing(id){
+    const updatedListingsArray = listings.filter(listing => listing.id !== id);
+    setListings(updatedListingsArray);
+  }
+
+  const filteredListings = listings.filter((listing) => {
+    return listing.description.toLowerCase().includes(search.toLowerCase());
+  });
+
+  const listingCards = filteredListings.map((listingsObj) => {
+    return <ListingCard 
+      key={listingsObj.id} 
+      listing={listingsObj}
+      onDeleteListing={handleDeleteListing}
+    />;
+  });
+
   return (
     <main>
       <ul className="cards">
-        {/* use the ListingCard component to display listings */}
+        {listingCards}
       </ul>
     </main>
   );
